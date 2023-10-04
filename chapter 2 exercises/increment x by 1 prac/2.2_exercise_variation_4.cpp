2bd40f945dae26b192e3cfa112ef9cf940603788
Kooji707 <vincent@Tris-MacBook-Air.local>
2023-10-04 09:56:04 -0700

    input int and increment by 1 (4 variations)

---
+++ chapter 2 exercises/increment x by 1 prac/2.2_exercise_variation_4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int X;
+    cout << "Please input integer\n";
+    cin >> X;
+    X++;
+    cout << "This integer incremented by 1 is " << X << endl;
+
+    return 0;
+}
