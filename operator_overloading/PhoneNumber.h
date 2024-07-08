8a92de0cb7907a3e012e7c98c26cd7cfc0a0af8a
Kooji707 <bboykooji@gmail.com>
2024-07-08 11:50:05 -0700

    operator overloading example

---
+++ operator_overloading/PhoneNumber.h
@@ -0,0 +1,20 @@
+#ifndef PHONENUMBER_H
+#define PHONENUMBER_H
+
+#include <iostream>
+using std::ostream;
+using std::istream;
+
+#include <string>
+using std::string;
+
+class PhoneNumber {
+    friend ostream &operator<<( ostream &, const PhoneNumber & );
+    friend istream &operator>>( istream &, PhoneNumber & );
+private:
+    string areaCode;
+    string exchange;
+    string line;
+};
+
+#endif
